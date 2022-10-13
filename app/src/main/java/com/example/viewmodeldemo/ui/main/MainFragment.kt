0483a064923cfbcb53f6a4881284d696e6667efb
jklom0326 <jklom0326@gmail.com>
2022-10-13 23:24:17 +0900

    observer

--- app/src/main/java/com/example/viewmodeldemo/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/viewmodeldemo/ui/main/MainFragment.kt
@@ -6,27 +6,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.viewmodeldemo.R
+import com.example.viewmodeldemo.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = MainFragment()
-    }
-
     private lateinit var viewModel: MainViewModel
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        binding.resultText.text = viewModel.getResult().toString()
+
+        binding.convertButton.setOnClickListener {
+            if (binding.dollarText.text.isNotEmpty()) {
+                viewModel.setAmount(binding.dollarText.text.toString())
+                binding.resultText.text = viewModel.getResult().toString()
+            } else {
+                binding.resultText.text = "no value"
+            }
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    companion object {
+        fun newInstance() = MainFragment()
+    }
 }
--- app/src/main/java/com/example/viewmodeldemo/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/viewmodeldemo/ui/main/MainViewModel.kt
@@ -1,6 +1,19 @@
 package com.example.viewmodeldemo.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+    private val usd_to_eu_rate = 0.74f
+    private var dollartext = ""
+    private var result:MutableLiveData<Float> = MutableLiveData()
+
+    fun setAmount(value: String) {
+        this.dollartext = value
+        result.value = value.toFloat() * usd_to_eu_rate
+    }
+
+    fun getResult(): MutableLiveData<Float> {
+        return result
+    }
 }
